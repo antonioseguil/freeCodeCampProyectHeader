e118b5c7f26ca80fb0497ed797df1179de798fd2
Antonio Seguil <antonioseguil@gmail.com>
2022-02-13 11:19:39 -0500

    Agregando requerimiento

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const { accepts } = require('express/lib/request');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -21,8 +22,11 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  let ipaddress = req.ip;
+  let language = req.headers['accept-language'];
+  let software = req.headers['user-agent'];
+  res.json({ipaddress, language, software});
 });
 
 
